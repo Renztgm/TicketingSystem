2a56f02804908bfac78506d3800081610e80b6d1
Renztgm <baconjhonlorenz@gmail.com>
2026-05-11 23:00:18 +0800

    Login page frontend added.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Landingpage from './landingPage.jsx'
+import LoginPage from './loginpage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Landingpage />
+    <LoginPage />
   </StrictMode>,
 )
