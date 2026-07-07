39846c64e68488e79c0523314e63cc0cbb166699
Delica, Riu L. <delica.riuict@gmail.com>
2026-07-07 20:04:00 +0800

    chore: Set trust proxy for the Express app

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const prisma = new PrismaClient();
 
+app.set('trust proxy', 1);
+
 app.use(cors());
 app.use(express.json());
 // Create the rate limit rule
